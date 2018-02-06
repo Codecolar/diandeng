3a4ac912b76f149d7f8c1485d765eef109f36319
xiaolang <xiaolang5293@gmail.com>
2018-02-06 13:45:04 +0800

    first push

---
+++ diandengroot/diandeng/yeelightcntl/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+import json
+import yeelightcntl.YeelightWifiBulbLanCtrl
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CntlYeelightBulb(View):
+
+
+    def post(self, request):
+        req_dict = json.loads(request.body)
+        token = req_dict["token"]
+        text = req_dict["text"]
+        trigger_word = req_dict["trigger_word"]
+
+        if token == "aPQk7fNoQV507Ygnx3OxR4gfqwQGROdDCfA0cHeQruk=":
+            if trigger_word == "dingdan":
+                YeelightWifiBulbLanCtrl.toggle_bulb(1)
+
+        return HttpResponse("")
